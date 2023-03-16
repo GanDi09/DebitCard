3f54d1a32c2cb4d4dd62242e58f76593ec230b47
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-16 17:19:04 +0300

    new commit

---
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.open
+
+public class DebitCardTest {
+
+
+
+    @Test
+    void shouldTest(){
+        open();
+
+    }
+}
