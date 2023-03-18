a34465840cbd43700e7e795755c00dbcce08ce2d
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-18 11:42:46 +0300

    new commit

--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -1,17 +1,23 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.open
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class DebitCardTest {
 
 
-
     @Test
     void shouldTest(){
-        open();
+        open("http://localhost:9999");
+        $("span[data-test-id=name] input").setValue("Генадий");
+        $("span[data-test-id=phone] input").setValue("+79057359359");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button__text")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).getText();
 
     }
 }
