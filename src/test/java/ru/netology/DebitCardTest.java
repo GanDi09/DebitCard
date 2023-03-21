3124932429489ca6af192af791ed858093bc2085
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2023-03-21 12:58:17 +0300

    new testkod

--- src/test/java/ru/netology/DebitCardTest.java
+++ src/test/java/ru/netology/DebitCardTest.java
@@ -1,27 +1,45 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class DebitCardTest {
-    private WebDriverManager driver;
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll(){
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp(){
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+
 
 
     @Test
-    void shouldTest() {
-        open("http://localhost:9999");
-        $("span[data-test-id=name] input").setValue("Генадий");
-        $("span[data-test-id=phone] input").setValue("+79057359359");
-        $("[data-test-id=agreement]").click();
-        $(By.className("button__text")).click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")).getText();
-
+    void shouldTest(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Генадий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7905735935");
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.cssSelector("[button__text]")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=rder-success]")).getText().trim();
+        assertEquals(expected, actual);
     }
 }
